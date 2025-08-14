2d642bfe047acec74f8ba5148b96b266fee696f0
Benjamin Nkem <benjaminnkemfrancis@gmail.com>
2025-08-14 11:52:57 +0100

    feat: sessions

--- src/routes/api/v1/task.routes.ts
+++ src/routes/api/v1/task.routes.ts
@@ -1,59 +1,59 @@
 import express from "express";
-import { TaskController } from "../../../controllers/tasks/task.controller";
 import { AppDataSource } from "../../../utils/data-source";
-import { Task } from "../../../entities/tasks/task.entity";
+import { Session } from "../../../entities/session/session.entity";
 import { validateBody } from "../../../middleware/validate.middleware";
-import { CreateTaskDto } from "../../../entities/tasks/dto/create-task.dto";
+import { CreateSessionDto } from "../../../entities/session/dto/create-session.dto";
+import { UpdateSessionDto } from "../../../entities/session/dto/update-session.dto";
 import {
   adminAuthMiddleware,
   authMiddleware,
   instructorAuthMiddleware,
 } from "../../../middleware/auth.middleware";
 import { User } from "../../../entities/user/user.entity";
+import { SessionController } from "../../../controllers/session/session.controller";
 
 const router = express.Router();
 
-const taskController = new TaskController(
-  AppDataSource.getRepository(Task),
+const sessionController = new SessionController(
+  AppDataSource.getRepository(Session),
   AppDataSource.getRepository(User)
 );
 
+// Create a new session (Instructor only)
 router.post(
   "/",
   instructorAuthMiddleware,
-  validateBody(CreateTaskDto),
-  taskController.createTask.bind(taskController)
+  validateBody(CreateSessionDto),
+  sessionController.createSession.bind(sessionController)
 );
 
+// Get all sessions (Admin only or filterable)
 router.get(
   "/",
   adminAuthMiddleware,
-  taskController.getAllTasks.bind(taskController)
+  sessionController.getAllSessions.bind(sessionController)
 );
 
-router.get(
-  "/user",
-  authMiddleware,
-  taskController.getUserTasks.bind(taskController)
-);
-
+// Get a single session by ID (Authenticated users)
 router.get(
   "/:id",
   authMiddleware,
-  taskController.getTaskById.bind(taskController)
+  sessionController.getSessionById.bind(sessionController)
 );
 
+// Update a session (Instructor only)
 router.put(
   "/:id",
   instructorAuthMiddleware,
-  validateBody(CreateTaskDto),
-  taskController.updateTask.bind(taskController)
+  validateBody(UpdateSessionDto),
+  sessionController.updateSession.bind(sessionController)
 );
 
+// Delete a session (Instructor only)
 router.delete(
   "/:id",
   instructorAuthMiddleware,
-  taskController.deleteTask.bind(taskController)
+  sessionController.deleteSession.bind(sessionController)
 );
 
 export default router;
