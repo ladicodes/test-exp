eba3d25dc7de87221e29e3a4537d903b462dd77b
Benjamin Nkem <benjaminnkemfrancis@gmail.com>
2025-08-14 12:12:13 +0100

    update: create session dto

--- src/entities/session/dto/create-session.dto.ts
+++ src/entities/session/dto/create-session.dto.ts
@@ -1,9 +1,21 @@
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { SessionCategory } from "../session.entity";
 
 export class CreateSessionDto {
+  @IsString()
   title: string;
+
+  @IsEnum(SessionCategory)
   category: SessionCategory;
+
+  @IsString()
   startTime: Date;
+
+  @IsString()
+  @IsOptional()
   endTime?: Date;
+
+  @IsString()
+  @IsOptional()
   description?: string;
 }
